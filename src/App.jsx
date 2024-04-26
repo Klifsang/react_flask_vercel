f3ea2d4f98ce9045c39684efae91cc4e8877b837
Klifsang <152657497+Klifsang@users.noreply.github.com>
2024-04-26 09:36:13 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   useEffect(() =>{
     const fetchData = async() =>{
-      const response = await fetch('http://127.0.0.1:5000')
+      const response = await fetch('/')
       console.log(response)
       const data = await response.json()
       setContent(data.title)
